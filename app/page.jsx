74e8d2c67f6479b8321ac5dadc2707c41e0ec5f6
gopala.s <gopala.s@pacewisdom.com>
2024-02-21 09:01:28 +0530

    commit

---
+++ app/page.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import TicketCard from "./(components)/TicketCard";
+
+const Home = () => {
+  return (
+    <div className="p-5">
+      <div className="lg:grid grid-cols-2 xl:grid-cols-4">
+        <TicketCard />
+        <TicketCard />
+        <TicketCard />
+        <TicketCard />
+        <TicketCard />
+        <TicketCard />
+        <TicketCard />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
